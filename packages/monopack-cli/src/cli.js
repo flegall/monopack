039247683f2b32728218fcffcad159df3984908d
Florent Le Gall <florent.legall@gmail.com>
2018-02-25 00:46:32 +0100

    feat(cli): Implement cli interface

--- packages/monopack-cli/src/cli.js
+++ packages/monopack-cli/src/cli.js
@@ -1,4 +1,30 @@
 // @flow
+import yargs from 'yargs';
+
 export function run() {
-  console.log('monopack ready !!');
+  const commandOption = yargs => {
+    yargs.positional('main', {
+      describe: 'The application entry point source file',
+      type: 'string',
+    });
+  };
+  const { argv } = yargs
+    .command('build main', 'Builds an application', commandOption)
+    .command('run main', 'Runs an application', commandOption)
+    .command('debug main', 'Runs an application in debug mode', commandOption)
+    .demandCommand(1, 'You need to enter a command')
+    .option('watch', {
+      alias: 'w',
+      default: false,
+      type: 'boolean',
+      describe: 'Enable watch mode',
+    })
+    .option('out-dir', {
+      alias: 'd',
+      type: 'string',
+      describe: 'Output directory (default into a temp dir)',
+      nargs: 1,
+    })
+    .strict();
+  console.log(argv);
 }
