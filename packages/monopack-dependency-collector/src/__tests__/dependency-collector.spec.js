195b2c0dea1d98d2f24a0aadc4db9e7b3afd81c7
Florent Le Gall <florent.legall@gmail.com>
2018-03-13 09:48:58 +0100

    feat(monopack-dependency-collector): Improve dependency collector

--- packages/monopack-dependency-collector/src/__tests__/dependency-collector.spec.js
+++ packages/monopack-dependency-collector/src/__tests__/dependency-collector.spec.js
@@ -1,13 +1,17 @@
 // @flow
+import fs from 'fs';
 import path from 'path';
 
-import { expect } from 'chai';
+import Bluebird from 'bluebird';
+import chai, { expect } from 'chai';
 import { aMonorepo, aPackage } from 'monopack-repo-builder';
 
 import DependencyCollector from '../index';
 
 // $FlowIgnore
 jest.setTimeout(30000);
+
+chai.config.truncateThreshold = 0;
 
 describe('dependency-collector', () => {
   it('should collect a top-level dependency installed with yarn from the monorepo root', async () => {
@@ -69,4 +73,190 @@ describe('dependency-collector', () => {
         });
       });
   });
+
+  it('should collect a sub-package dependency installed with yarn from a sub-package', async () => {
+    // given
+    await aMonorepo()
+      .named('root')
+      .withEmptyConfigFile()
+      .withWorkspacesEnabled()
+      .withPackages(
+        aPackage()
+          .named('sub')
+          .withDependencies({ lodash: '4.17.5' })
+      )
+      .execute(async ({ root }) => {
+        const collector = new DependencyCollector(root);
+
+        // when
+        collector.collectDependency(
+          'lodash',
+          path.join(root, 'packages', 'sub')
+        );
+
+        // then
+        const result = await collector.resolveDependencies();
+        expect(result).to.deep.equal({
+          type: 'SUCCESS_FULLY_DETERMINISTIC',
+          yarnLockFileToCopy: path.join(root, 'yarn.lock'),
+          dependencies: [
+            {
+              packageName: 'lodash',
+              version: '4.17.5',
+            },
+          ],
+        });
+      });
+  });
+
+  it(`when a dependency is required but not installed an error is returned`, async () => {
+    // given
+    await aMonorepo()
+      .named('root')
+      .withEmptyConfigFile()
+      .withWorkspacesEnabled()
+      .execute(async ({ root }) => {
+        const collector = new DependencyCollector(root);
+
+        // when
+        collector.collectDependency('lodash', path.join(root));
+
+        // then
+        const result = await collector.resolveDependencies();
+        expect(result).to.deep.equal({
+          type: 'FAILURE_UNDECLARED_DEPENDENCIES',
+          undeclaredDependencies: [{ dependency: 'lodash' }],
+        });
+      });
+  });
+
+  it(`when two dependencies are installed and required from each sub-package with yarn workspaces,
+    if the dependencies are compatible
+    a single version is used`, async () => {
+    // given
+    await aMonorepo()
+      .named('root')
+      .withEmptyConfigFile()
+      .withWorkspacesEnabled()
+      .withPackages(
+        aPackage()
+          .named('sub1')
+          .withDependencies({ lodash: '^4.17.4' }),
+        aPackage()
+          .named('sub2')
+          .withDependencies({ lodash: '^4.17.5' })
+      )
+      .execute(async ({ root }) => {
+        const collector = new DependencyCollector(root);
+
+        // when
+        collector.collectDependency(
+          'lodash',
+          path.join(root, 'packages', 'sub1')
+        );
+        collector.collectDependency(
+          'lodash',
+          path.join(root, 'packages', 'sub2')
+        );
+
+        // then
+        const result = await collector.resolveDependencies();
+        expect(result).to.deep.equal({
+          type: 'SUCCESS_FULLY_DETERMINISTIC',
+          yarnLockFileToCopy: path.join(root, 'yarn.lock'),
+          dependencies: [
+            {
+              packageName: 'lodash',
+              version: '^4.17.4',
+            },
+          ],
+        });
+      });
+  });
+
+  it(`when two dependencies are installed and required from each sub-package with yarn workspaces,
+      if the dependencies are not compatible
+      an error is returned`, async () => {
+    // given
+    await aMonorepo()
+      .named('root')
+      .withEmptyConfigFile()
+      .withWorkspacesEnabled()
+      .withPackages(
+        aPackage()
+          .named('sub1')
+          .withDependencies({ lodash: '4.17.4' }),
+        aPackage()
+          .named('sub2')
+          .withDependencies({ lodash: '4.17.5' })
+      )
+      .execute(async ({ root }) => {
+        const collector = new DependencyCollector(root);
+
+        // when
+        collector.collectDependency(
+          'lodash',
+          path.join(root, 'packages', 'sub1')
+        );
+        collector.collectDependency(
+          'lodash',
+          path.join(root, 'packages', 'sub2')
+        );
+
+        // then
+        const result = await collector.resolveDependencies();
+        expect(result).to.deep.equal({
+          type: 'FAILURE_NEEDS_DEPENDENCY_CONFLICT_RESOLUTION',
+          conflicts: {
+            lodash: [
+              {
+                packageVersion: '4.17.4',
+                context: path.join(root, 'packages', 'sub1'),
+              },
+              {
+                packageVersion: '4.17.5',
+                context: path.join(root, 'packages', 'sub2'),
+              },
+            ],
+          },
+        });
+      });
+  });
+
+  it(`when dependencies are resolved without yarn.lock files,
+    the dependencies are resolved, but their installation will not be deterministic`, async () => {
+    // given
+    await aMonorepo()
+      .named('root')
+      .withEmptyConfigFile()
+      .withWorkspacesEnabled()
+      .withDependencies({ lodash: '4.17.5' })
+      .execute(async ({ root }) => {
+        await unlink(path.join(root, 'yarn.lock'));
+        const collector = new DependencyCollector(root);
+
+        // when
+        collector.collectDependency('lodash', path.join(root));
+
+        // then
+        const result = await collector.resolveDependencies();
+        expect(result).to.deep.equal({
+          type: 'SUCCESS_NOT_DETERMINISTIC_NO_YARN_LOCKS',
+          dependencies: [
+            {
+              packageName: 'lodash',
+              version: '4.17.5',
+            },
+          ],
+        });
+      });
+  });
+
+  xit(`when dependencies are resolved from multiple yarn.lock files,
+    the dependencies are resolved, but their installation will not be deterministic`, async () => {});
+
+  xit(`when two different versions of dependencies are installed and required from each sub-package with no yarn.lock,
+      an error is returned`, async () => {});
 });
+
+const unlink: string => Promise<void> = Bluebird.promisify(fs.unlink);
--- packages/monopack-dependency-collector/src/index.js
+++ packages/monopack-dependency-collector/src/index.js
@@ -2,11 +2,10 @@
 import fs from 'fs';
 import path from 'path';
 
+import _ from 'lodash';
 import { Semaphore } from 'await-semaphore';
 import Bluebird from 'bluebird';
 import * as lockfile from '@yarnpkg/lockfile';
-
-type ThunkToThunk<T> = (() => Promise<T>) => Promise<T>;
 
 export type CollectedDependencies =
   | {
@@ -21,20 +20,13 @@ export type CollectedDependencies =
       type: 'FAILURE_NEEDS_DEPENDENCY_CONFLICT_RESOLUTION',
       conflicts: {
         [packageName: string]: { packageVersion: string, context: string }[],
-      }[],
+      },
     }
   | {
       type: 'FAILURE_UNDECLARED_DEPENDENCIES',
-      undeclaredDependencies: (
-        | {
-            type: 'UNDECLARED',
-            dependency: string,
-          }
-        | {
-            type: 'DECLARED_AS_DEV_DEPENDENCY',
-            dependency: string,
-          }
-      )[],
+      undeclaredDependencies: {
+        dependency: string,
+      }[],
     }
   | {
       type: 'SUCCESS_NOT_DETERMINISTIC_MULTIPLE_YARN_LOCKS',
@@ -54,8 +46,7 @@ export type CollectedDependencies =
 
 export default class DependencyCollector {
   monorepoRoot: string;
-  promises: Promise<void>[] = [];
-  resolvedDependencies: { packageName: string, declaredVersion: string }[] = [];
+  promises: Promise<MaybeResolvedDependency>[] = [];
   yarnLockFiles: string[] = [];
 
   constructor(monorepoRoot: string) {
@@ -66,16 +57,39 @@ export default class DependencyCollector {
     this.promises.push(
       semaphore.use(async () => {
         const packages = await this._getPackageJsons(context);
-        for (const pkg of packages) {
+
+        for (let i = 0; i < packages.length; i++) {
+          const pkg = packages[i];
           const packageJsonVersion = pkg.dependencies[packageName];
+          const packageAndVersion = `${packageName}@${packageJsonVersion}`;
+
+          let yarnLockMatchingPackage: ?YarnLock;
+          for (let j = i; j < packages.length; j++) {
+            const yarnLock = packages[j].lockFile;
+            if (yarnLock && yarnLock.dependencies[packageAndVersion]) {
+              yarnLockMatchingPackage = yarnLock;
+              break;
+            }
+          }
+
           if (packageJsonVersion) {
-            this.resolvedDependencies.push({
+            return {
+              type: 'RESOLVED',
               packageName,
+              context,
               declaredVersion: packageJsonVersion,
-            });
-            break;
+              resolvedVersion: yarnLockMatchingPackage
+                ? yarnLockMatchingPackage.dependencies[packageAndVersion]
+                    .version
+                : null,
+              yarnLockPath: yarnLockMatchingPackage
+                ? yarnLockMatchingPackage.path
+                : null,
+            };
           }
         }
+
+        return { type: 'NOT_RESOLVED', packageName, context };
       })
     );
   }
@@ -97,7 +111,10 @@ export default class DependencyCollector {
         const fixedLockFileContent = lockFileContent.replace(/\r/g, '');
 
         const yarnLock = lockfile.parse(fixedLockFileContent);
-        lockFile = { dependencies: yarnLock.object };
+        lockFile = {
+          path: context,
+          dependencies: yarnLock.object,
+        };
       }
 
       localPackageJsons.push({
@@ -119,29 +136,159 @@ export default class DependencyCollector {
   }
 
   async resolveDependencies(): Promise<CollectedDependencies> {
-    await Promise.all(this.promises);
+    const maybeResolvedDependencies = await Promise.all(this.promises);
+
+    const unresolvedDeps: NotResolvedDependency[] = (maybeResolvedDependencies.filter(
+      ({ type }) => type === 'NOT_RESOLVED'
+    ): any);
+    if (unresolvedDeps.length > 0) {
+      return {
+        type: 'FAILURE_UNDECLARED_DEPENDENCIES',
+        undeclaredDependencies: unresolvedDeps.map(({ packageName }) => ({
+          dependency: packageName,
+        })),
+      };
+    }
+
+    const resolvedDeps: ResolvedDependency[] = (maybeResolvedDependencies.filter(
+      ({ type }) => type === 'RESOLVED'
+    ): any);
+
+    const isFullyResolved = ({
+      resolvedVersion,
+      yarnLockPath,
+    }: ResolvedDependency) => resolvedVersion !== null && yarnLockPath !== null;
+
+    const notFullyResolvedDeps: NotFullyResolvedDependency[] = (resolvedDeps.filter(
+      not(isFullyResolved)
+    ): any);
+    if (notFullyResolvedDeps.length > 0) {
+      const dependenciesByPackageName: {
+        [string]: NotFullyResolvedDependency[],
+      } = _.groupBy(notFullyResolvedDeps, ({ packageName }) => packageName);
+
+      return {
+        type: 'SUCCESS_NOT_DETERMINISTIC_NO_YARN_LOCKS',
+        dependencies: Object.keys(dependenciesByPackageName)
+          .map(key => dependenciesByPackageName[key])
+          .map(([firstDependency]) => firstDependency)
+          .map(({ packageName, declaredVersion }) => ({
+            packageName,
+            version: declaredVersion,
+          })),
+      };
+    }
+
+    const fullyResolvedDeps: FullyResolvedDependency[] = (resolvedDeps.filter(
+      isFullyResolved
+    ): any);
+
+    const resolvedDepsByPackageName: {
+      [string]: FullyResolvedDependency[],
+    } = _.groupBy(fullyResolvedDeps, ({ packageName }) => packageName);
+
+    const conflicts: Conflicts = Object.keys(resolvedDepsByPackageName).reduce(
+      (accumulator: Conflicts, packageName) => {
+        const conflicts: Conflicts = {};
+
+        const dependencies = resolvedDepsByPackageName[packageName];
+        const dependenciesByResolvedVersion: {
+          [string]: FullyResolvedDependency[],
+        } = _.groupBy(dependencies, ({ resolvedVersion }) => resolvedVersion);
+
+        if (Object.keys(dependenciesByResolvedVersion).length > 1) {
+          const conflict = [];
+
+          for (const key in dependenciesByResolvedVersion) {
+            const [firstDependency] = dependenciesByResolvedVersion[key];
+            conflict.push({
+              context: firstDependency.context,
+              packageVersion: firstDependency.declaredVersion,
+            });
+          }
+
+          conflicts[packageName] = conflict;
+        }
+
+        return { ...accumulator, ...conflicts };
+      },
+      {}
+    );
+
+    if (Object.keys(conflicts).length > 0) {
+      return {
+        type: 'FAILURE_NEEDS_DEPENDENCY_CONFLICT_RESOLUTION',
+        conflicts,
+      };
+    }
+
     return {
       type: 'SUCCESS_FULLY_DETERMINISTIC',
       yarnLockFileToCopy: path.join(this.monorepoRoot, 'yarn.lock'),
-      dependencies: this.resolvedDependencies.map(
-        ({ packageName, declaredVersion }) => ({
+      dependencies: Object.keys(resolvedDepsByPackageName)
+        .map(key => resolvedDepsByPackageName[key])
+        .map(([firstDependency]) => firstDependency)
+        .map(({ packageName, declaredVersion }) => ({
           packageName,
           version: declaredVersion,
-        })
-      ),
+        })),
     };
   }
 }
 
+type YarnLock = {
+  path: string,
+  dependencies: {
+    [string]: {
+      version: string,
+    },
+  },
+};
 type PackageJson = {
   path: string,
   dependencies: { [string]: string },
   devDependencies: { [string]: string },
-  lockFile: null | { dependencies: { [string]: { version: string } } },
+  lockFile: null | YarnLock,
 };
 
+type FullyResolvedDependency = {
+  type: 'RESOLVED',
+  packageName: string,
+  context: string,
+  declaredVersion: string,
+  resolvedVersion: string,
+  yarnLockPath: string,
+};
+type NotFullyResolvedDependency = {
+  type: 'RESOLVED',
+  packageName: string,
+  context: string,
+  declaredVersion: string,
+  resolvedVersion: null,
+  yarnLockPath: null,
+};
+type ResolvedDependency = {
+  type: 'RESOLVED',
+  packageName: string,
+  context: string,
+  declaredVersion: string,
+  resolvedVersion: string | null,
+  yarnLockPath: string | null,
+};
+type NotResolvedDependency = {
+  type: 'NOT_RESOLVED',
+  packageName: string,
+  context: string,
+};
+type MaybeResolvedDependency = ResolvedDependency | NotResolvedDependency;
+
+type Conflicts = {
+  [packageName: string]: { packageVersion: string, context: string }[],
+};
+
+type ThunkToThunk<T> = (() => Promise<T>) => Promise<T>;
 const semaphore: {
-  use: ThunkToThunk<void>,
+  use: ThunkToThunk<MaybeResolvedDependency>,
 } = new Semaphore(10);
 
 const readFile: (string, 'utf8') => Promise<string> = Bluebird.promisify(
@@ -157,3 +304,7 @@ const exists: string => Promise<boolean> = async file => {
     return false;
   }
 };
+
+function not<T>(predicate: (t: T) => boolean): T => boolean {
+  return t => !predicate(t);
+}
